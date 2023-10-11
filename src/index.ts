6a9f054c770dd89ac5cb68c92a26325c44c6823b
Erik Hermansson <erik.hermansson@swedwise.se>
2023-10-11 15:22:34 +0200

    Added FileActionProgress to export.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Files from './components/Files';
+import { FileActionProgress } from './components/FileActionProgress';
 import filesReducer from './services/fileActions';
 import { fileActionWatcherSaga } from './sagas/fileActionsSaga';
 
 export {
     Files,
+    FileActionProgress,
     filesReducer,
     fileActionWatcherSaga
 }
