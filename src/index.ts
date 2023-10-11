cfb4eecbe9b7c11ca452e7368c6d03c4df5da2fa
Erik Hermansson <erik.hermansson@swedwise.se>
2023-10-11 15:32:38 +0200

    Added SelectFolderModal and SelectImageModal to export.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import Files from './components/Files';
 import { FileActionProgress } from './components/FileActionProgress';
+import { SelectImageModal } from './components/SelectImageModal';
+import { SelectFolderModal } from './components/SelectFolderModal';
 import filesReducer from './services/fileActions';
 import { fileActionWatcherSaga } from './sagas/fileActionsSaga';
 
 export {
     Files,
     FileActionProgress,
+    SelectImageModal,
+    SelectFolderModal,
     filesReducer,
     fileActionWatcherSaga
 }
