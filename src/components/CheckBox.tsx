f36b54e1c78b78b409accbcca9b051e4dbcfef4e
Erik Hermansson <erik.hermansson@swedwise.se>
2024-01-29 10:54:13 +0100

    Changed Checkbox to use FontAwesomeIcon component.

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,18 +1,22 @@
 ﻿import { useState } from 'react';
-import classnames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck as solidCircleCheck } from '@fortawesome/free-solid-svg-icons'
+import { faCircleCheck as hollowCircleCheck, faCircle } from '@fortawesome/free-regular-svg-icons'
 
 export function CheckBox({ isChecked, className, onClick }) {
     const [isHovering, setIsHovering] = useState(false);
 
+    const icon = isChecked
+        ? solidCircleCheck
+        : isHovering
+            ? hollowCircleCheck
+            : faCircle;
+
     return (
-        <i className={classnames(className, {
-            "fa-solid fa-circle-check": isChecked,
-            "fa-regular fa-circle-check": !isChecked && isHovering,
-            "fa-regular fa-circle": !isChecked && !isHovering
-        })}
+        <FontAwesomeIcon icon={icon}
             onMouseOver={() => setIsHovering(true)}
             onMouseOut={() => setIsHovering(false)}
             onClick={onClick}
-        ></i>
+        ></FontAwesomeIcon>
     )
 }
